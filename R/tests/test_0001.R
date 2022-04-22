144a0109e49a69ccaa73dd6b25712f9d6ff15cb2
Jacquie Tran <jacquie.tran@gmail.com>
2022-04-22 19:23:51 +1200

    tests

---
+++ R/tests/test_0001.R
@@ -0,0 +1,75 @@
+# Load libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(ambient)
+
+# Modifiable parameters --------------------------------------------------------
+
+iteration_id <- "test_0001"
+seed_num <- 127810
+initial_grid_size <- 500
+warp_factor <- 100 # lower = more warping
+line_colour <- "#999999"
+dot_colour <- "#DDDDDD"
+bg_colour <- "#222222"
+
+# Make some noise --------------------------------------------------------------
+
+# Generate data
+set.seed(seed_num)
+grid <- long_grid(
+  seq(0, 20, length.out = initial_grid_size),
+  seq(0, 20, length.out = initial_grid_size)) %>%
+  mutate(
+    curl = curl_noise(gen_value, x = x, y = y)) %>%
+  purrr::reduce(data.frame) %>%
+  rename(x = out, y = elt, curl_x = x, curl_y = y) %>%
+  mutate(
+    x_warped = x + (curl_x / warp_factor),
+    y_warped = y + (curl_y / warp_factor),
+    size = sample(seq(0.2, 1.5, by = 0.1), n(), replace = TRUE),
+    subset = sample(1:20, n(), replace = TRUE))
+
+set.seed(seed_num)
+grid_cols <- grid %>%
+  distinct(x) %>%
+  mutate(
+    colour = case_when(
+      x < 0.4            ~ sample(
+        c("#E4F1F1", "#006D77", "#83C5BE"), n(), replace = TRUE,
+        prob = c(0.6, 0.3, 0.1)),
+      x >= 0.4 & x < 0.8 ~ sample(
+        c("#E4F1F1", "#006D77", "#83C5BE"), n(), replace = TRUE,
+        prob = c(0.1, 0.6, 0.3)),
+      TRUE               ~ sample(
+        c("#E4F1F1", "#006D77", "#83C5BE"), n(), replace = TRUE,
+        prob = c(0.3, 0.1, 0.6))))
+
+grid_with_colours <- left_join(grid, grid_cols, by = "x")
+      
+
+# Build plot -------------------------------------------------------------------
+
+ggplot() +
+  #geom_path(
+  #  data = grid,
+  #  aes(x = x_warped, y = y_warped),
+  #  colour = line_colour, size = 0.2, linetype = "dotted") +
+  geom_path(
+    data = grid_with_colours,
+    aes(x = x_warped, y = y_warped, colour = colour, size = size)) +
+  scale_size_identity() +
+  scale_colour_identity() +
+  coord_equal(expand = FALSE) +
+  theme_void() +
+  theme(
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_colour, colour = bg_colour),
+    plot.margin     = margin(100,100,100,100, unit = "pt"))
+
+# Export to file ---------------------------------------------------------------
+
+ggsave(
+  here::here(glue::glue("img/tests/{`iteration_id`}.png")),
+  device = "png", width = 6000, height = 6000, units = "px", dpi = 600)
